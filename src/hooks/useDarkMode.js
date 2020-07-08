021ab5d77c2f0318778a1c38a85a0f38b3979e65
shanon <13fritsh@gmail.com>
2020-07-08 16:03:36 -0500

    fixed my toggle

---
+++ src/hooks/useDarkMode.js
@@ -0,0 +1,20 @@
+import useLocalStorage from './useLocalStorage';
+
+const useDarkMode = (key, initialValue) => {
+    const [value, setValue] = useLocalStorage(key, initialValue);
+    console.log(value);
+    const app = document.querySelector('.App')
+    const toggleMode = e => {
+        e.preventDefault();
+        setValue(!value);
+        if (value === false) {
+            app.classList.add('dark-mode')
+        } else if (value === true) {
+            app.classList.remove('dark-mode')
+        }
+      };
+
+    return [value, toggleMode]
+};
+
+export default useDarkMode;
